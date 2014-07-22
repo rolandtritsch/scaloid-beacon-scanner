4f5ff7e423bac53c45a378b607a8a7524974322f
Roland Tritsch <roland@tritsch.org>
2014-07-22 12:40:55 +0100

    First working version

--- src/main/scala/BeaconScanner.scala
+++ src/main/scala/BeaconScanner.scala
@@ -4,22 +4,20 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AdapterView
 
 import org.scaloid.common._
 
+import scala.collection.mutable
+
 class BeaconScanner extends SActivity {
   override implicit val loggerTag = LoggerTag("BeaconScanner")
-  private val MSG_CONNECT_AND_DISPLAY = 100
 
   private var bluetoothAdapter: BluetoothAdapter = null
-  private var lv: SListView = null
+  private val beaconMap = new mutable.HashMap[String, Int]
 
   // views ...
   private def startView: View = new SVerticalLayout {
@@ -35,12 +33,23 @@ class BeaconScanner extends SActivity {
       contentView = startView
     )
 
-    val beacons = SArrayAdapter(Array("Searching ..."))
-    lv = new SListView()
+    info(s"scanning for BLE devices ...")
+    bluetoothAdapter.startLeScan(listenForDevices)
+    Thread.sleep(5000)
+    bluetoothAdapter.stopLeScan(listenForDevices)
+
+    val beacons = SArrayAdapter(
+      beaconMap.keys.map(k => {
+        s"${k} -> [${beaconMap(k)}]"
+      }).toArray
+    )
+
+    val lv = new SListView()
     lv.setAdapter(beacons)
     lv.onItemClick((parent: AdapterView[_], v: View, position: Int, row: Long) => {
       toast(s">${position}/${row}<")
     })
+
     this += lv
   } padding 20.dip
 
@@ -57,82 +66,6 @@ class BeaconScanner extends SActivity {
     debug("Leave - onCreate")
   }
 
-  override def onResume: Unit = {
-    super.onResume
-    debug("Enter - onResume")
-    resumeScan
-    debug("Leave - onResume")
-  }
-
-  override def onPause: Unit = {
-    super.onPause
-    debug("Enter - onPause")
-    stopScan
-    debug("Leave - onPause")
-  }
-
-  // helpers and handlers ...
-  private val handler = new Handler {
-    override def handleMessage(msg: Message): Unit = {
-      msg.what match {
-        case MSG_CONNECT_AND_DISPLAY => {
-          debug(s"Enter - MSG_CONNECT_AND_DISPLAY - ${lv}")
-
-          if(lv != null) {
-            val beacons = lv.adapter.asInstanceOf[ArrayAdapter[String]]
-            beacons.notifyDataSetChanged
-          }
-
-          debug(s"Leave - MSG_CONNECT_AND_DISPLAY")
-        }
-      }
-    }
-  }
-
-  private val resumeRunnable = new Runnable {
-    override def run: Unit = {
-      resumeScan
-    }
-  }
-
-  private val suspendRunnable = new Runnable {
-    override def run: Unit = {
-      suspendScan
-    }
-  }
-
-  private def stopScan: Unit = {
-    debug("Enter - stopScan")
-
-    handler.removeCallbacks(resumeRunnable)
-    handler.removeCallbacks(suspendRunnable)
-    bluetoothAdapter.stopLeScan(listenForDevices)
-
-    debug("Leave - stopScan")
-  }
-
-  private def resumeScan: Unit = {
-    debug("Enter - resumeScan")
-
-    bluetoothAdapter.startLeScan(listenForDevices)
-    setProgressBarIndeterminateVisibility(true)
-    handler.postDelayed(suspendRunnable, 1000)
-
-    debug("Leave - resumeScan")
-  }
-
-  private def suspendScan: Unit = {
-    debug("Enter - suspendScan")
-
-    bluetoothAdapter.stopLeScan(listenForDevices)
-    setProgressBarIndeterminateVisibility(false)
-
-    handler.sendEmptyMessage(MSG_CONNECT_AND_DISPLAY)
-    handler.postDelayed(resumeRunnable, 1000)
-
-    debug("Leave - suspendScan")
-  }
-
   // the BLE stuff ...
   val listenForDevices = new BluetoothAdapter.LeScanCallback {
     override def onLeScan(device: BluetoothDevice, rssi: Int, sr: Array[Byte]): Unit = {
@@ -140,8 +73,7 @@ class BeaconScanner extends SActivity {
       info(s"Found device >${device.getAddress}/${device.getName}< @ >${rssi}< ...")
       verbose(s"With ScanRecord >${ScanRecord.dump(sr)}< ...")
 
-      val beacons = lv.adapter.asInstanceOf[ArrayAdapter[String]]
-      beacons.add(s"${device.getAddress} -> ${rssi}")
+      beaconMap += (device.getAddress -> rssi)
 
       debug("Leave - onLeScan")
     }
